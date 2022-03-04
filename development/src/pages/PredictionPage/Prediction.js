ea013d618b718b78b8fd550e51571552633b7d51
suramisam <87141576+suramisam@users.noreply.github.com>
2022-03-04 21:14:36 +0530

    deleted some unnecessary files

--- development/src/pages/PredictionPage/Prediction.js
+++ development/src/pages/PredictionPage/Prediction.js
@@ -1,13 +1,12 @@
 import React from "react";
 import MatchDetails from './MatchDetails';
-import TabPanel from "./TabPanel";
+
 
 
 const prediction = () => {
     return (
         <div className="heder">
             <MatchDetails/>
-            <TabPanel/>
         </div>
         
 
