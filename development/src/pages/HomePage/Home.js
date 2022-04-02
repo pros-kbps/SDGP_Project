fd6527a6f9a73bb34449b2ac3a7131889f1ed471
pavi-ravi26 <pavithraieni.20200530@iit.ac.lk>
2022-04-02 23:51:23 +0530

    timeline added

--- development/src/pages/HomePage/Home.js
+++ development/src/pages/HomePage/Home.js
@@ -3,13 +3,15 @@ import PredictForm from './PredictForm'
 import {Grid } from '@material-ui/core';
 import Container from '@mui/material/Container';
 import Header from "../../components/Header/Header"
-
+import Timeline from './Timeline/timeline'
 export default function Home() {
     
     return (
     <div className="header 2">
         <Header/>
+        <Timeline/>
         <PredictForm/>
+        
     </div>
 
     );
---
+++ development/src/pages/HomePage/Timeline/test.js
@@ -0,0 +1,36 @@
+import '../../../styles/Timeline.css'
+import timelineData from './Data'
+
+
+const TimelineItem = ({ data }) => (
+    <div className="timeline-item">
+        <div className="timeline-item-content">
+            <span className="tag" style={{ background: data.category.color }}>
+                {data.category.tag}
+            </span>
+            <time>{data.date}</time>
+            <p>{data.text}</p>
+            {data.link && (
+                <a
+                    href={data.link.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    {data.link.text}
+                </a>
+            )}
+            <span className="circle" />
+        </div>
+    </div>
+);
+
+const Timeline = () =>
+    timelineData.length > 0 && (
+        <div className="timeline-container">
+            {timelineData.map((data, idx) => (
+                <TimelineItem data={data} key={idx} />
+            ))}
+        </div>
+    );
+            
+export default Timeline;
