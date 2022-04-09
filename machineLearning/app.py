9daf2de925b14e06fd91f9f17fee4720678af781
suramisam <87141576+suramisam@users.noreply.github.com>
2022-04-09 17:46:27 +0530

    ml output

--- machineLearning/app.py
+++ machineLearning/app.py
@@ -1,43 +1,37 @@
-from flask import Flask,render_template,request
-import json
+from flask import Flask,request
+from flask_restful import Api, Resource, reqparse
 import pickle
 import numpy as np
 import requests as rq
-import PredictionForm from '../pages/HomePage/PredictForm'
+import json
+# import PredictionForm from '../pages/HomePage/PredictForm'
 app = Flask(__name__)
 api = Api(app)
 
 parser = reqparse.RequestParser()
 parser.add_argument('data')
 
-class Model(Resource):
-    def post(self):
-        args = parser.parse_args()
-        X = np.array(json.loads(args['data']))
-        prediction = model.predict(X)
 
-api.add_resource(Model, '/prediction')
+@app.route('/predict',methods=['POST'])
+def predict(): 
+        json1 = request.json
+        
+        # args = parser.parse_args()
+        # # args = args.replace('"','\\"')
+        print(json1)
+        # X = np.array(json.loads(args['data']))
+        # print(X)
+        input = [json1['team1'],json1["team2"],json1["venue"],json1["tossDecision"],json1["tossWinner"]]
+        final = [np.array(input)] 
+
+        
+        output=model.predict(final)
+        print (output);
+        return json.dumps(output.tolist())
+
 
 if __name__ == '__main__':
     # Load model
     model = pickle.load(open('model.pkl','rb'))
 
     app.run(debug=True)
-
-@app.route('/')
-def home():
-    return render_template('')
-
-@app.route('/',methods=['POST'])
-def predict(): 
-        response = rq.get("http://localhost:8080/prediction/predict")
-        resJson = response.json()
-        team1 = resJson.team1.id
-        team1 = resJson.team2.id
-        toss_decision= resJson.
-
-        final = [np.array()] 
-
-        
-        prediction=model.predict(final)
-        print(f"The winner would be: {list(team_encodings.keys())[list(team_encode_dict['team1'].values()).index(output)]}")
